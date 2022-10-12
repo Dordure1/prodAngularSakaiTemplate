0c418bb6dfb1dcc3ff4b37310b5992a8f9239a40
dorian <dorian.vanhoutte1@gmail.com>
2022-10-12 09:43:54 +0200

    home routing module

--- src/app/production/home/home-routing.module.ts
+++ src/app/production/home/home-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomecComponent } from './homec/homec.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomecComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
