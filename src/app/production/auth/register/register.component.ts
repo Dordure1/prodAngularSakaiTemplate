757493f583df781dcd42befd76c0006bbe3de5ab
dorian <dorian.vanhoutte1@gmail.com>
2022-10-14 10:49:17 +0200

    reactive forms

--- src/app/production/auth/register/register.component.ts
+++ src/app/production/auth/register/register.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { generateMyForm } from '../../shared/formsValidator/register.form';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm! : {
+    email : string,
+    coopName: string, 
+    password : string, 
+    Vpassword: string, 
+    city : string, 
+    address : string, 
+    number : number
+  }
+
+  myForm : FormGroup = generateMyForm(this.fb , this.client)
+
+  constructor(
+    private fb : FormBuilder,
+    private client : HttpClient
+  ) { }
 
   ngOnInit(): void {
   }
+
+  sendForm()
+  {
+    if(this.myForm.status =="VALID")
+    {
+      console.log("it's ok")
+      console.log(this.myForm.status)
+    }
+    else{
+      console.log("notOk");
+      console.log(this.myForm)
+    }
+  }
+
 
 }
---
+++ src/app/production/shared/formsValidator/register.form.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from "@angular/common/http";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { matchPasswordValidator } from "./validators/matchPassword.validators";
+
+export function generateMyForm(fb : FormBuilder, httpC : HttpClient) : FormGroup{
+
+    return fb.group(
+    /*controls : */{
+        email : ["", {
+                        validators : [Validators.required, Validators.email],
+                        asyncValidators : [],
+                        /*updateOn : 'blur'*/
+                    }
+                ],
+        coopName : ["",{
+                        validators : [Validators.required, Validators.minLength(3)]
+        }],
+
+        password : ["", {
+                            validators : [Validators.required, Validators.minLength(6)]
+                        }
+                    ],
+
+        Vpassword : ["",{
+                            validators :  [Validators.required, Validators.minLength(6)]
+                        }
+                    ],
+        
+        city : ["", {
+                            validators : [Validators.required]
+                    }
+                ],
+        
+        address : ["", {
+                            validators : [Validators.required]
+                        }
+                    ],
+
+        number : ["", {
+                            validators : [Validators.required]
+                        }
+                ]
+    },
+    // {
+    //     //options  
+    //     validators : [matchPasswordValidator],
+    //     updateOn : 'blur'
+    // }
+
+    )   
+}
+
