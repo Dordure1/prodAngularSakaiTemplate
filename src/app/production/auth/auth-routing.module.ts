e8ebbc130842f05bf8400c074086c1be8f9bec36
Dorian <dorian.vanhoutte1@gmail.com>
2022-10-18 09:16:09 +0200

    loginuser and register user module creation

--- src/app/production/auth/auth-routing.module.ts
+++ src/app/production/auth/auth-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './loginCoop/login.component';
+import { RegisterComponent } from './registerCoop/register.component';
 import { YouarecComponent } from './youare/youarec/youarec.component';
 
 const routes: Routes = [
--- src/app/production/auth/auth.module.ts
+++ src/app/production/auth/auth.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './loginCoop/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './registerCoop/register.component';
 import { PasswordModule } from 'primeng/password';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox';
@@ -12,6 +12,8 @@ import { ButtonModule } from 'primeng/button';
 import { YouarecComponent } from './youare/youarec/youarec.component';
 import {SplitterModule} from 'primeng/splitter';
 import {CardModule} from 'primeng/card';
+import { LoginuserComponent } from './loginuser/loginuser/loginuser.component';
+import { RegisteruserComponent } from './registeruser/registeruser/registeruser.component';
 
 
 
@@ -20,7 +22,9 @@ import {CardModule} from 'primeng/card';
     LoginComponent,
     LogoutComponent,
     RegisterComponent,
-    YouarecComponent
+    YouarecComponent,
+    LoginuserComponent,
+    RegisteruserComponent
   ],
   imports: [
     CommonModule,
